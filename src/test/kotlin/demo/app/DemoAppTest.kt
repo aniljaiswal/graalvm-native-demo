8bcae572dd4594d44d60579ff746caedb58329a0
Anil Jaiswal <er.anil.jaiswal@gmail.com>
2022-05-08 14:05:11 +0530

    Initial commit

---
+++ src/test/kotlin/demo/app/DemoAppTest.kt
@@ -0,0 +1,19 @@
+package demo.app
+import io.micronaut.runtime.EmbeddedApplication
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import jakarta.inject.Inject
+
+@MicronautTest
+class DemoAppTest {
+
+    @Inject
+    lateinit var application: EmbeddedApplication<*>
+
+    @Test
+    fun testItWorks() {
+        Assertions.assertTrue(application.isRunning)
+    }
+
+}
